b35cc6fa123b18dedb2cf043e25a46e837f8c1d7
behzad <behzad.m.777666@gmail.com>
2015-02-03 15:14:28 +0330

    db complete

---
+++ src/com/example/chibepazam/DAO/FoodDAO.java
@@ -0,0 +1,81 @@
+package com.example.chibepazam.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.chibepazam.db.MySQLHelper;
+import com.example.chibepazam.models.Food;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class FoodDAO {
+	private SQLiteDatabase fooddb;
+	private MySQLHelper dbHelper;
+	private String[] allColumns = { MySQLHelper.COLUMN_NAME_ENTRY_ID,
+			MySQLHelper.COLUMN_NAME_FOOD_NAME,
+			MySQLHelper.COLUMN_NAME_COOK_TIME,
+			MySQLHelper.COLUMN_NAME_COOK_PEOPLE,
+			MySQLHelper.COLUMN_NAME_INGREDIENT, MySQLHelper.COLUMN_NAME_RECIPE };
+
+	public FoodDAO(Context context) {
+		dbHelper = new MySQLHelper(context);
+	}
+
+	public void open() throws SQLException {
+		fooddb = dbHelper.getWritableDatabase();
+	}
+
+	public void close() {
+		dbHelper.close();
+	}
+
+	public void createFood(String name, String time, String people,
+			String ingredient, String recipe) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLHelper.COLUMN_NAME_FOOD_NAME, name);
+		values.put(MySQLHelper.COLUMN_NAME_COOK_TIME, time);
+		values.put(MySQLHelper.COLUMN_NAME_COOK_PEOPLE, people);
+		values.put(MySQLHelper.COLUMN_NAME_INGREDIENT, ingredient);
+		values.put(MySQLHelper.COLUMN_NAME_RECIPE, recipe);
+		long insertId = fooddb.insert(MySQLHelper.TABLE_NAME, null, values);
+	}
+
+	public void deleteFood(Food food) {
+		long id = food.getId();
+		System.out.println("food deleted with id: " + id);
+		fooddb.delete(MySQLHelper.TABLE_NAME, MySQLHelper.COLUMN_NAME_ENTRY_ID
+				+ " = " + id, null);
+	}
+
+	public List<Food> getAllFoods() {
+		List<Food> foods = new ArrayList<Food>();
+
+		Cursor cursor = fooddb.query(MySQLHelper.TABLE_NAME, allColumns, null,
+				null, null, null, null);
+
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Food food = cursorToFood(cursor);
+			foods.add(food);
+			cursor.moveToNext();
+		}
+		// make sure to close the cursor
+		cursor.close();
+		return foods;
+	}
+
+	private Food cursorToFood(Cursor cursor) {
+		Food food = new Food();
+		food.setId(cursor.getLong(0));
+		food.setName(cursor.getString(1));
+		food.setTime(cursor.getString(2));
+		food.setPeople(cursor.getString(3));
+		food.setIngredient(cursor.getString(4));
+		food.setRecipe(cursor.getString(5));
+		return food;
+	}
+}
---
+++ src/com/example/chibepazam/db/MySQLHelper.java
@@ -0,0 +1,47 @@
+package com.example.chibepazam.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLHelper extends SQLiteOpenHelper {
+	public static final String TABLE_NAME = "food";
+	public static final String COLUMN_NAME_ENTRY_ID = "food_id";
+	public static final String COLUMN_NAME_FOOD_NAME = "food_name";
+	public static final String COLUMN_NAME_COOK_TIME = "cook_time";
+	public static final String COLUMN_NAME_COOK_PEOPLE = "cook_people";
+	public static final String COLUMN_NAME_INGREDIENT = "ingredient";
+	public static final String COLUMN_NAME_RECIPE = "recipe";
+
+	private static final String DATABASE_NAME = "food.db";
+	private static final int DATABASE_VERSION = 1;
+
+	// Database creation sql statement
+	private static final String DATABASE_CREATE = "create table " + TABLE_NAME
+			+ "(" + COLUMN_NAME_ENTRY_ID
+			+ " integer primary key autoincrement, " + COLUMN_NAME_FOOD_NAME
+			+ " text, " + COLUMN_NAME_COOK_TIME + " text, "
+			+ COLUMN_NAME_COOK_PEOPLE + " text, " + COLUMN_NAME_INGREDIENT
+			+ " text," + COLUMN_NAME_RECIPE + " text);";
+
+	public MySQLHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(MySQLHelper.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+		onCreate(db);
+	}
+
+}
