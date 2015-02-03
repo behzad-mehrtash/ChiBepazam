e4712d6487dc5ae0f2157574b6362414a7873817
safa ahmadian <safaahmadian@safas-MacBook-Pro.local>
2015-02-03 18:41:41 +0330

    UI-1

--- gen/com/example/chibepazam/R.java
+++ gen/com/example/chibepazam/R.java
@@ -1080,6 +1080,10 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int B1=0x7f05003e;
+        public static final int B2=0x7f050042;
+        public static final int ET1=0x7f05003d;
+        public static final int TV1=0x7f050040;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,10 +1095,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1141,6 +1146,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int value=0x7f05003f;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
---
+++ src/com/example/chibepazam/FisrtPage.java
@@ -0,0 +1,5 @@
+package com.example.chibepazam;
+
+public class FisrtPage {
+
+}
