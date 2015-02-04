b35939bfe20fcf5810b929a01348913de24c5f6e
safa ahmadian <safaahmadian@safas-MacBook-Pro.local>
2015-02-04 17:39:27 +0330

    land

--- src/com/example/chibepazam/MainActivity.java
+++ src/com/example/chibepazam/MainActivity.java
@@ -1,16 +1,8 @@
 package com.example.chibepazam;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import com.example.chibepazam.R;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
